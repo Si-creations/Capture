19c43f959e6dbb2f7b0a350c8e38890abf7aa90f
Si-creations <s.mrocek@gmail.com>
2023-10-02 15:24:29 +0200

    .

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -77,7 +77,7 @@ const Awards = styled.div`
   margin: 5rem 10rem;
   align-items: center;
   justify-content: space-around;
-  @media (max-width: 1500px) {
+  @media (max-width: 780px) {
             display:block;
             margin: 2rem 2rem;
           }
